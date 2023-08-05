25997d761395e4863292dcc1df51713ab2572e17
김상구 <spair0039@gmail.com>
2023-08-06 00:18:03 +0900

    buttonLW style

--- src/styles/Button.jsx
+++ src/styles/Button.jsx
@@ -4,3 +4,14 @@ export const ButtonUB = styled.a`
     color: #71727B;
     font-size: 12px;
 `
+
+export const ButtonLW = styled.button`
+    background-color: #4983F6;
+    width: 100%;
+    padding: 10px 20px;
+    color: #FFF;
+    font-size: 20px;
+    font-weight: 400;
+    border: 0;
+    border-radius: 1242px;
+`
