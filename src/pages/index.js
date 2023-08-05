f6f74b8dc2d42b105fc213032438fb289cdfe9eb
김상구 <spair0039@gmail.com>
2023-08-05 21:20:44 +0900

    mainpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
+import TotalContainer from "@/components/TotalContainer";
+import Navigation from "@/components/nav";
+import SusList from "@/components/suslist";
+import { styled } from "styled-components";
+
 export default function Home() {
     return (
         <div>
-            adsf
+            <NameContainer>
+                탬버린
+            </NameContainer>
+            <SusList/>
+            <Navigation/>
+            <TotalContainer num={800}/>
         </div>
-        )
-    }
-    
+    )
+}
+
+const NameContainer = styled.div`
+    padding: 10px 40px;
+    background-color: #FF7372;
+    font-size: 16px;
+    width: fit-content;
+    border-radius:1293481029px;
+    color: #fff;
+`
