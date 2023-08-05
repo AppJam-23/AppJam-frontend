6217fd901358a830c20b674783d68118dc0bd4b8
김상구 <spair0039@gmail.com>
2023-08-06 02:14:37 +0900

    zd

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import Input from "@/components/input";
-import { ButtonLW } from "@/styles/Button";
+import { ButtonLW, ButtonUB } from "@/styles/Button";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { styled } from "styled-components";
 
 export default function Home(){
     const router = useRouter();
@@ -27,10 +28,28 @@ export default function Home(){
     }
 
     return(
-        <div>
-            <img src="/ihavenojua.png"/>
-            <Input label='참가 코드' onChange={(e)=>setind(e.target.value)} value={ind} placeholder="참가 코드를 입력하세요" />
-            <ButtonLW onClick={route}>참여하기</ButtonLW>
-        </div>
+        <Center>
+            <MainCon>
+                <img src="/ihavenojua.png"/>
+                <Input label='참가 코드' onChange={(e)=>setind(e.target.value)} value={ind} placeholder="참가 코드를 입력하세요" />
+                <ButtonLW onClick={route}>참여하기</ButtonLW>
+                <ButtonUB onClick={()=> router.push('/create')}>행사 생성</ButtonUB>
+            </MainCon>
+        </Center>
     )
 }
+
+const MainCon = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    width: 280px;
+`
+const Center = styled.div`
+    width: 100%;
+    height: 50vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
