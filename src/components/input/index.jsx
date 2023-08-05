f41db1d7eb0fde7b6619df87ad5211c1bc097544
김상구 <spair0039@gmail.com>
2023-08-05 23:53:08 +0900

    input styling

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -7,18 +7,27 @@ export default function Input({onChange, value, label}){
                 <img src="/rar.png"/>
                 {label}
             </Lab>
-            <input id={label} value={value} onChange={onChange}/>
+            <Inp id={label} value={value} onChange={onChange}/>
         </InputCon>
     )
 }
 
 const InputCon = styled.div`
     display: flex;
-    gap: 4px;
+    gap: 8px;
     flex-direction: column;
 `
 
 const Lab = styled.label`
     color: #4983F6;
+    gap: 4px;
+    display: flex;
+    align-items: center;
+    font-weight: 800;
+`
 
+const Inp = styled.input`
+    padding: 14px;
+    border-radius: 8px;
+    border: 1px solid #71727B;
 `
